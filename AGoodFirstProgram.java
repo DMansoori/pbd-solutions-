ecca168469903b632b9aab17904c94287353e90d
DMansoori <36383418+DMansoori@users.noreply.github.com>
2018-02-16 11:09:55 -0500

    Add files via upload

---
+++ AGoodFirstProgram.java
@@ -0,0 +1,10 @@
+public class AGoodFirstProgram
+{
+   public static void main( String[] args )
+   {
+   System.out.println( "Hello World");
+   System.out.println( "Hello again");
+   System.out.println( "I like typing this" );
+   System.out.println( "This is fun."); 
+    	}
+}
