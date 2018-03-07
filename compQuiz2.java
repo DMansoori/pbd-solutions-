3727cfff3eb0130e0b48a2add5a66cdf84eb13a9
DMansoori <36383418+DMansoori@users.noreply.github.com>
2018-03-07 10:13:55 -0500

    Create compQuiz2.java

---
+++ compQuiz2.java
@@ -0,0 +1,17 @@
+public static void main(String[] args) {
+  int x = 10;
+  int y = 30;
+  
+  if (x > y) {
+  System.out.println("X is greater than Y);
+  }
+  
+  if (x < y) {
+  System.out.println("Y is greater than C);
+  }
+  
+  if (x == y) {
+  System.out.println("X is equalt to Y);
+  }
+  
+  }
